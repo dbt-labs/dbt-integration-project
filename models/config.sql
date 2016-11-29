2e7df5bf0babe60fc30ac1634325e3181c53d57a
Drew Banin <drew@fishtownanalytics.com>
2016-11-29 12:48:24 -0500

    add bool config option

--- models/config.sql
+++ models/config.sql
@@ -7,5 +7,6 @@
 
 select *,
     '{{ var("config_1") }}'::text as c1,
-    '{{ var("config_2") }}'::text as c2
+    '{{ var("config_2") }}'::text as c2,
+    case when {{ var("bool_config") }} then 'was true' else 'was false' end as some_bool
 from simple_dependency_006.seed
