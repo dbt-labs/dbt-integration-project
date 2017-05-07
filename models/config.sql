f4f07c64eda720134df80f15fd4fc8cfb7b2291a
Erin Ogilvy <erin@fishtownanalytics.com>
2017-05-07 18:32:27 -0400

    don't hardcode schemas

--- models/config.sql
+++ models/config.sql
@@ -9,4 +9,4 @@ select *,
     '{{ var("config_1") }}'::text as c1,
     '{{ var("config_2") }}'::text as c2,
     case when {{ var("bool_config") }} then 'was true' else 'was false' end as some_bool
-from simple_dependency_006.seed
+from {{ this.schema }}.seed
